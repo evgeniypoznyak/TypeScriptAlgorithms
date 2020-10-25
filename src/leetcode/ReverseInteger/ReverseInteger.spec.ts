d66d031facc766cc9773db767e0d8bdd72351d43
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-10-25 18:50:24 -0500

    ReverseInteger

---
+++ src/leetcode/ReverseInteger/ReverseInteger.spec.ts
@@ -0,0 +1,19 @@
+import { reverse } from './ReverseInteger';
+
+describe('ReverseInteger', () => {
+    it('Should return 321', () => {
+        const actual = reverse(123);
+        const expected = 321;
+        expect(actual).toStrictEqual(expected);
+    });
+    it('Should return 0 if max positive', () => {
+        const actual = reverse(1534236469);
+        const expected = 0;
+        expect(actual).toStrictEqual(expected);
+    });
+    it('Should return 0 if max negative', () => {
+        const actual = reverse(-1563847412);
+        const expected = 0;
+        expect(actual).toStrictEqual(expected);
+    });
+});
---
+++ src/leetcode/ReverseInteger/ReverseInteger.ts
@@ -0,0 +1,14 @@
+export const reverse = (x: number): number => {
+    const minimum = -2147483648;
+    const maximum = 2147483647;
+
+    if (x === 0) {
+        return 0;
+    }
+
+    const result = Math.sign(x) * parseInt(x.toString().split('').reverse().join(''));
+    if (result < minimum || result > maximum) {
+        return 0;
+    }
+    return result;
+};
