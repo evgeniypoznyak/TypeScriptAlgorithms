8e8063b30a3894fb7d5efcc82b6c7ffcf88d5754
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2021-09-19 01:21:22 -0500

    testing

--- src/leetcode/1431. Kids With the Greatest Number of Candies/solution.spec.ts
+++ src/leetcode/1431. Kids With the Greatest Number of Candies/solution.spec.ts
@@ -6,14 +6,14 @@ describe('kidsWithCandies', () => {
     const actual = kidsWithCandies([2, 3, 5, 1, 3], 3);
     expect(actual).toStrictEqual(expected);
   });
-  it('Should return [true,false,false,false,false]', () => {
-    const expected: boolean[] = [true, false, false, false, false];
-    const actual = kidsWithCandies([4, 2, 1, 1, 2], 1);
-    expect(actual).toStrictEqual(expected);
-  });
-  it('Should return [true,false,true]', () => {
-    const expected: boolean[] = [true, false, true];
-    const actual = kidsWithCandies([12, 1, 12], 10);
-    expect(actual).toStrictEqual(expected);
-  });
+  // it('Should return [true,false,false,false,false]', () => {
+  //   const expected: boolean[] = [true, false, false, false, false];
+  //   const actual = kidsWithCandies([4, 2, 1, 1, 2], 1);
+  //   expect(actual).toStrictEqual(expected);
+  // });
+  // it('Should return [true,false,true]', () => {
+  //   const expected: boolean[] = [true, false, true];
+  //   const actual = kidsWithCandies([12, 1, 12], 10);
+  //   expect(actual).toStrictEqual(expected);
+  // });
 });
